a81acc2227a8a876b746bb99d55ec94ab9f6c87c
Anton Punko <ant.punko@gmail.com>
2018-08-15 14:04:38 +0300

    removed gatsby starter code

---
+++ en/path----75ad7aa8d5b18d34e294.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{102:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Anton Punko"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----75ad7aa8d5b18d34e294.js.map
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Anton Punko',
   },
   plugins: ['gatsby-plugin-react-helmet'],
   pathPrefix: `/en`,
