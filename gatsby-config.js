a03287f2acf1edc6700632c4b95ebcbab1ee0a66
Anton Punko <ant.punko@gmail.com>
2018-08-15 19:21:22 +0300

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,5 @@
 module.exports = {
   siteMetadata: {
-    title: 'Anton Punko',
-  },
-  plugins: ['gatsby-plugin-react-helmet'],
-  pathPrefix: `/en`,
-}
-module.exports = {
-  siteMetadata: {
     title: 'Web developer and chess lover',
   },
   pathPrefix: `/en`,
